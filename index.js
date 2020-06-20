cd3c71268453158aa9c385e4c5179d0377c518a7
Adam Rumschlag <adam.rumschlag@gmail.com>
2020-06-20 10:09:13 -0400

    firstcommit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* FROM HTML Old NavBar
+<body>
+    <nav class="navbar">
+        <ul>
+          <li><a href="#contact" class="nav-link">Contact</a></li>
+          <li><a href="#resume" class="nav-link">Resume</a></li>
+          <li><a href="#projects" class="nav-link">Projects</a></li>
+          <li><a href="#aboutme" class="nav-link">About Me</a></li>
+        </ul>
+      </nav>
+</body>
+
+
+
+FROM OLD PROJECTS SECTION IN HTML
+<div id="projects" style="height: 75vh;">
+        <h2>Projects</h2>
+        <p>Projects will go here.</p>>
+      </div>
+
+FOOTER
+
+
+
+*/
+
